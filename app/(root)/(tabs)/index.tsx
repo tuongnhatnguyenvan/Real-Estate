252de19ccef0413bb53545e4a717ada9e877eddc
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2025-01-11 21:03:46 +0700

    set up fonts and assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold py-10 text-lg">Hello World</Text>
+      <Text className="font-rubik py-10 text-3xl">Welcom to my app</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
